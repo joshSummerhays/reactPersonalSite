304855ba4188f230f4dabefdd038492b59561b4b
Josh Summerhays <russiansummer@gmail.com>
2025-06-25 11:10:47 -0600

    feat: convert from nextjs to react/vite

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  distDir: 'out',
-  output: 'export',
-};
-
-export default nextConfig;
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Pardon the dust, this site is currently under construction</h1>
+    </>
+  )
+}
+
+export default App
