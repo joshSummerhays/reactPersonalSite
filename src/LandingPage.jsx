c71f44e92b625ef75f69c31c173a1d6d98279076
Josh Summerhays <russiansummer@gmail.com>
2025-06-26 18:10:35 -0600

    feat: landing page rollin

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -3,16 +3,18 @@ import React from 'react';
 export const LandingPage = () => (
     <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', background: '#f5f7fa' }}>
         <div className='nes-mario'></div>
-        <h1>It's-a-me, Josh!</h1>
-        <div style={{backgroundColor: "#212529", padding: "1rem 0"}}>
+        <h1 style={{ margin: "2rem"}}>It's-a-me, Josh!</h1>
+        <div style={{backgroundColor: "#212529", padding: "1rem 1rem"}}>
             <h4 style={{ color: "#fff", textAlign: "center" }}>Continue?</h4>
             <label>
                 <input type="radio" className="nes-radio is-dark" name="answer-dark" checked />
                 <span>Yes</span>
             </label>
             <label>
-                <input type="radio" className="nes-radio is-dark" name="answer-dark" />
-                <span>No</span>
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
+                    <input type="radio" className="nes-radio is-dark" name="answer-dark" />
+                    <span>No</span>
+                </a>
             </label>
         </div>
     </div>
